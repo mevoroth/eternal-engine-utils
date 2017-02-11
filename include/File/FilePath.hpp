a536bb2aecd67af096a1c60e9cfcf498e1527f7d
Daniel Mevoroth Quach <daniel@quach.info>
2017-02-11 15:53:59 +0100

    FilePath generalization

---
+++ include/File/FilePath.hpp
@@ -0,0 +1,30 @@
+#ifndef _FILE_PATH_HPP_
+#define _FILE_PATH_HPP_
+
+#include <string>
+#include <vector>
+
+namespace Eternal
+{
+	namespace File
+	{
+		using namespace std;
+
+		class FilePath
+		{
+		public:
+			enum FileType
+			{
+				SHADERS = 0,
+				FILE_TYPE_COUNT
+			};
+			static void Register(_In_ const string& FolderPath, _In_ const FileType& Type);
+			static string Find(_In_ const string& FileName, _In_ const FileType& Type);
+
+		private:
+			static vector<string> _FolderPaths[FILE_TYPE_COUNT];
+		};
+	}
+}
+
+#endif
---
+++ src/File/FilePath.cpp
@@ -0,0 +1,39 @@
+#include "File/FilePath.hpp"
+
+#include "Macros/Macros.hpp"
+#include "Types/Types.hpp"
+#include "File/FileFactory.hpp"
+
+using namespace Eternal::File;
+using namespace Eternal::Types;
+
+vector<string> FilePath::_FolderPaths[FILE_TYPE_COUNT];
+
+string FilePath::Find(_In_ const string& FileName, _In_ const FileType& Type)
+{
+	ETERNAL_ASSERT(Type < FILE_TYPE_COUNT);
+	ETERNAL_ASSERT(FileName.size() > 0);
+	vector<string>& FolderPaths = _FolderPaths[Type];
+
+	bool FileFound = false;
+	string FullPath;
+	for (u32 FolderIndex = 0; !FileFound && FolderIndex < FolderPaths.size(); ++FolderIndex)
+	{
+		string TempFullPath = FolderPaths[FolderIndex] + FileName;
+		FileFound = FileExists(TempFullPath);
+		if (FileFound)
+			FullPath = TempFullPath;
+	}
+	if (FileFound)
+		return FullPath;
+	
+	ETERNAL_ASSERT(false);
+	return string();
+}
+
+void FilePath::Register(_In_ const string& FolderPath, _In_ const FileType& Type)
+{
+	ETERNAL_ASSERT(Type < FILE_TYPE_COUNT);
+	ETERNAL_ASSERT(FolderPath.size() > 0);
+	_FolderPaths[Type].push_back(FolderPath);
+}
