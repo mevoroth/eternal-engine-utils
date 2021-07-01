fdb6b4e16301aba335faf697338b7eb2fdec0b71
Daniel Quach <daniel@quach.info>
2021-07-02 01:14:41 +0200

    Align function

--- include/Math/Math.hpp
+++ include/Math/Math.hpp
@@ -11,6 +11,12 @@ namespace Eternal
 		}
 
 		template<typename T>
+		constexpr T Align(T Dividend, T AlignmentSize)
+		{
+			return DivideRoundUp<T>(Dividend, AlignmentSize) * AlignmentSize;
+		}
+
+		template<typename T>
 		bool IsPowerOfTwo(T Number)
 		{
 			return Number && ((Number & (Number - 1)) == 0);
