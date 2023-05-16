b0c29aef1268821f44022999e399d75a65c6cd0b
Daniel Quach <darkskana@gmail.com>
2023-05-16 14:03:11 +0200

    default cfile define

--- include/UtilsSettings.hpp
+++ include/UtilsSettings.hpp
@@ -4,7 +4,9 @@
 #define ETERNAL_USE_DXMATH_TYPES	(1)
 #endif
 
+#ifndef ETERNAL_USE_CFILE
 #define ETERNAL_USE_CFILE			(1)
+#endif
 
 #ifndef ETERNAL_USE_STD_PARALLEL
 #define ETERNAL_USE_STD_PARALLEL	(1)
