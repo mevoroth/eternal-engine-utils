8ae42178c158e67a05b4106ce42bd66de184d036
Daniel Quach <daniel@quach.info>
2024-11-24 02:04:12 +0100

    Refactoring math library

--- include/Math/Math.hpp
+++ include/Math/Math.hpp
@@ -14,84 +14,84 @@ namespace Eternal
 		static constexpr float M_TO_KM			= 1.0f / 1000.0f;
 
 		template<typename T>
-		constexpr T DivideRoundUp(_In_ T Dividend, _In_ T Divisor)
+		constexpr T DivideRoundUp(_In_ T InDividend, _In_ T InDivisor)
 		{
-			return (Dividend + Divisor - 1) / Divisor;
+			return (InDividend + InDivisor - 1) / InDivisor;
 		}
 
 		template<typename T>
-		constexpr T Align(_In_ T Dividend, _In_ T AlignmentSize)
+		constexpr T Align(_In_ T InDividend, _In_ T InAlignmentSize)
 		{
-			return DivideRoundUp<T>(Dividend, AlignmentSize) * AlignmentSize;
+			return DivideRoundUp<T>(InDividend, InAlignmentSize) * InAlignmentSize;
 		}
 
 		template<typename T>
-		bool IsPowerOfTwo(_In_ T Number)
+		bool IsPowerOfTwo(_In_ T InNumber)
 		{
-			return Number && ((Number & (Number - 1)) == 0);
+			return InNumber && ((InNumber & (InNumber - 1)) == 0);
 		}
 
 		template<typename T>
-		T Sign(_In_ const T& X)
+		T Sign(_In_ const T& InX)
 		{
-			return T(T(0) < X) - (X < T(0));
+			return T(T(0) < InX) - (InX < T(0));
 		}
 
 		template<typename T>
-		T Abs(_In_ const T& X)
+		T Abs(_In_ const T& InX)
 		{
-			return std::abs(X);
+			return std::abs(InX);
 		}
 
 		template<typename T>
-		T Min(_In_ const T& A, _In_ const T& B)
+		T Min(_In_ const T& In, _In_ const T& InB)
 		{
-			return std::min<T>(A, B);
+			return std::min<T>(In, InB);
 		}
 
 		template<typename T>
-		T Max(_In_ const T& A, _In_ const T& B)
+		T Max(_In_ const T& InA, _In_ const T& InB)
 		{
-			return std::max(A, B);
+			return std::max(InA, InB);
 		}
 
 		template<typename T>
-		T Clamp(_In_ const T& X, _In_ const T& A, _In_ const T& B)
+		T Clamp(_In_ const T& InX, _In_ const T& InA, _In_ const T& InB)
 		{
-			return Max(Min(X, B), A);
+			return Max(Min(InX, InB), InA);
 		}
 
 		template<typename T>
-		T SafeClamp(_In_ const T& X, _In_ const T& A, _In_ const T& B)
+		T SafeClamp(_In_ const T& InX, _In_ const T& InA, _In_ const T& InB)
 		{
-			if (A < B)
-				std::swap(A, B);
+			if (InA < InB)
+				std::swap(InA, InB);
 
-			return Clamp(X, A, B);
+			return Clamp(InX, InA, InB);
 		}
 
 		template<typename T>
-		T Saturate(_In_ const T& X)
+		T Saturate(_In_ const T& InX)
 		{
-			return Clamp(X, T(0.0f), T(1.0f));
+			return Clamp(InX, T(0.0f), T(1.0f));
 		}
 
 		template<typename T>
-		T Lerp(_In_ const T& A, _In_ const T& B, _In_ float X)
+		T Lerp(_In_ const T& InA, _In_ const T& InB, _In_ float InX)
 		{
-			return  A + (B - A) * X;
+			return  InA + (InB - InA) * InX;
 		}
 
 		template<typename T>
-		T InverseLerp(_In_ const T& A, _In_ const T& B, _In_ const T& Y)
+		T InverseLerp(_In_ const T& InA, _In_ const T& InB, _In_ const T& InY)
 		{
-			return (Y - A) / (B - A);
+			return (InY - InA) / (InB - InA);
 		}
 
 		template<typename T>
-		T Log2(_In_ const T& X)
+		T Log2(_In_ const T& InX)
 		{
-			return static_cast<T>(log2(X));
+			return static_cast<T>(log2(InX));
 		}
 	}
 }
