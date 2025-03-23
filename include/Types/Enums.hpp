5efccfbd465adb1eba4ddb118607ee0ac96ab733
Daniel Quach <daniel@quach.info>
2025-03-23 16:43:54 +0000

    is_enum check on enum helpers

--- include/Types/Enums.hpp
+++ include/Types/Enums.hpp
@@ -7,12 +7,14 @@ namespace Eternal
 		template<typename EnumType>
 		constexpr uint32_t ToUInt(EnumType InEnumValue)
 		{
+			ETERNAL_STATIC_ASSERT(std::is_enum<EnumType>(), "Enum type required");
 			return static_cast<uint32_t>(InEnumValue);
 		}
 
 		template<typename EnumType>
 		constexpr int ToInt(EnumType InEnumValue)
 		{
+			ETERNAL_STATIC_ASSERT(std::is_enum<EnumType>(), "Enum type required");
 			return static_cast<int>(InEnumValue);
 		}
 	}
