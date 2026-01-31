d74e2ee0fb6140b903b1d8fb12e54212012df8dd
Daniel Quach <daniel@quach.info>
2026-01-31 22:52:35 +0000

    Allow empty folder for FilePath::Register

--- src/File/FilePath.cpp
+++ src/File/FilePath.cpp
@@ -67,7 +67,6 @@ namespace Eternal
 		void FilePath::Register(_In_ const string& InFolderPath, _In_ const FileType& InFileType)
 		{
 			ETERNAL_ASSERT(static_cast<uint32_t>(InFileType) < static_cast<uint32_t>(FileType::FILE_TYPE_COUNT));
-			ETERNAL_ASSERT(InFolderPath.size() > 0);
 			_FolderPaths[static_cast<int32_t>(InFileType)].push_back(InFolderPath);
 		}
 
